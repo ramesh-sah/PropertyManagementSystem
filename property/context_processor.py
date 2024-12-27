4a4ee1c2933353aa69901e075c0308e789b45352
Ramesh Kumar Sah <88158556+ramesh-sah@users.noreply.github.com>
2024-12-27 13:40:27 +0545

    added the context processor

---
+++ property/context_processor.py
@@ -0,0 +1,14 @@
+from .models import *
+def properties(request):
+    properties = Property.objects.all()  # Fetch all properties
+    propertyamenities = PropertyAmenities.objects.all()  # Fetch all properties amenities
+    propertylisiting = PropertyListingDetail.objects.all()  # Fetch all properties details
+    propertymedia = PropertyMedia.objects.all()  # Fetch all properties details
+    propertyaddress = PropertyAddress.objects.all()
+    return {
+        'properties': properties,  # Make it available in templates
+        'amenities': propertyamenities,
+        'listings': propertylisiting,
+        'medias': propertymedia,
+        'addresses': propertyaddress,
+    }
